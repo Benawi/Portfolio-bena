50256d1f3ffe570152fe1e3deea8d9e8b7b21c16
Habtamu Alemayehu <habtamu.alemayehudema@dashenbanksc.com>
2025-07-07 10:09:17 +0300

    add baseurl

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ const App = () => {
     <BrowserRouter  basename="/Portfolio-bena">
       {navBar.show && <Navbar ref={titleRef} />}
       <Routes>
-        <Route path="/" exact element={<Home ref={titleRef} />} />
+        <Route path="/"  element={<Home ref={titleRef} />} />
       </Routes>
       {/* {false && <Route path="/blog" exact component={Blog} />}
       {false && <Route path="/blog/:id" component={BlogPost} />} */}
